6a2143e73d9a344ec460fe4d25d5d206f8554696
hyun722ls <hyun722ls@gmail.com>
2022-05-12 15:50:03 +0900

    fix: darkmode

--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -25,7 +25,7 @@ export default {
   },
   methods: {
     toggleDarkMode() {
-      if (localStorage.theme === 'dark') {
+      if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.remove('dark')
         localStorage.theme = 'light'
         this.isDark = false
